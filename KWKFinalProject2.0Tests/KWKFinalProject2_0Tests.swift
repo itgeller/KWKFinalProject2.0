d3ea352a35e93bb083542da4da43652f22a7ad3d
Isabella Geller <isabellageller29@gmail.com>
2021-08-04 13:08:42 -0400

    Initial Commit

---
+++ KWKFinalProject2.0Tests/KWKFinalProject2_0Tests.swift
@@ -0,0 +1,33 @@
+//
+//  KWKFinalProject2_0Tests.swift
+//  KWKFinalProject2.0Tests
+//
+//  Created by Isabella Geller on 8/4/21.
+//
+
+import XCTest
+@testable import KWKFinalProject2_0
+
+class KWKFinalProject2_0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
